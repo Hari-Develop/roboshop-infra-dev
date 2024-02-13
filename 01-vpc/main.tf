4820abc1d7489524057c8c125f39c7a9e9b5ed0c
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2024-02-13 21:21:13 +0530

    file changes

--- 01-vpc/main.tf
+++ 01-vpc/main.tf
@@ -4,7 +4,7 @@ module "roboshop" {
   project_name = var.project_name
   environment = var.environment
   common_tags = var.common_tags
-  vpc_tags = var.vpc_hari
+  vpc_tags = var.vpc_tags
 
   # public subnet
   public_subnets_cidr = var.public_subnets_cidr
